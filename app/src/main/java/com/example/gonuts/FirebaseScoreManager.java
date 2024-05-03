23bcf8efdf96bd31ce9a8e9acf6595f6d87df3f8
nickkschmidt <nnnnschmidt@gmail.com>
2024-05-02 20:57:28 -0700

    GoNuts buttons + collision done, ghost collision in progress, game complete, score and finish screen in progress, everything except score screen complete
    *Final code*

--- app/src/main/java/com/example/gonuts/FirebaseScoreManager.java
+++ app/src/main/java/com/example/gonuts/FirebaseScoreManager.java
@@ -5,11 +5,24 @@ import android.util.Log;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+/**
+ * Manages score data in the Firebase Realtime Database.
+ *
+ * This class provides methods to interact with the Firebase Realtime Database
+ * for storing and retrieving score data.
+ */
+
 public class FirebaseScoreManager {
 
     private final FirebaseDatabase database;
     private static DatabaseReference scoresRef;
 
+    /**
+     * Constructs a new FirebaseScoreManager instance.
+     *
+     * Initializes the Firebase database and obtains a reference to the "Scores"
+     * node in the database.
+     */
     public FirebaseScoreManager() {
         // Initialize Firebase
         database = FirebaseDatabase.getInstance();
@@ -18,6 +31,12 @@ public class FirebaseScoreManager {
         scoresRef = database.getReference("Scores");
     }
 
+    /**
+     * Adds a score to the specified node in the Firebase database.
+     *
+     * @param nodeName The name of the node in which to add the score.
+     * @param score The score to be added to the database.
+     */
     public void addScorefire(String nodeName, int score) {
         scoresRef.child(nodeName).push().setValue(score);
     }
